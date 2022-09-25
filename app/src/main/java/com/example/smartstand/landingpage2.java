81559d4366254f10ab762ddc4eb8acbf614a7c95
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-09-25 13:46:38 +0530

    Splash Screen and Landing Pages Done.

---
+++ app/src/main/java/com/example/smartstand/landingpage2.java
@@ -0,0 +1,35 @@
+package com.example.smartstand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class landingpage2 extends AppCompatActivity {
+    Button btn;
+    TextView skip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landingpage2);
+        btn=findViewById(R.id.button2);
+        skip=findViewById(R.id.skip);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),landingpage3.class);
+                startActivity(intent);
+            }
+        });
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),landingpage3.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smartstand/landingpage3.java
@@ -0,0 +1,25 @@
+package com.example.smartstand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class landingpage3 extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landingpage3);
+        btn=findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),landingpage2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
