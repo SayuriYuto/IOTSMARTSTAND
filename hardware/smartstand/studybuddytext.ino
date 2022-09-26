4eb83a2b4788c704736632b331ce36e978b94d22
SayuriYuto <dhuriamey02@gmail.com>
2022-09-26 22:58:20 +0530

    text display

---
+++ hardware/smartstand/studybuddytext.ino
@@ -0,0 +1,9 @@
+void studybuddytext()
+{
+    display.fillRect(0, 0, 58, 7, BLACK);
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(1, 1);
+    display.println("STUDY BUDDY");
+    display.display();
+}
